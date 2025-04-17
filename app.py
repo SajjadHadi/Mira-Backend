38ea3a4c1c04a78821c7a1efd909a64bea31bf0e
SajjadHadi <sajjadhadi77@gmail.com>
2025-04-17 13:34:39 +0200

    Fix access origin for local Angular apps.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.routes import router as auth_router
 from db import check_database
@@ -23,6 +24,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Mental Disorder Detection API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # <-- Angular dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(llm_router)
